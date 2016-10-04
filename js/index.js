c3ecd0fdac06dbf8ade17eafa512f9c0a1824060
chenx <bufubaoni@163.com>
2016-10-04 20:02:41 +0800

    index

--- js/index.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Alex on 2016/10/3.
- */
-var HelloWorld = React.creatClass()
---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Alex on 2016/10/3.
+ */
+const { Button } = require("rsuite");
+var Button = require("rsuite").Button;
+define(['rsuite'],function(Suite){
+    var Button = Suite.Button;
+});
+
+ReactDOM.render(<Button>Button</Button>,mountNode);
