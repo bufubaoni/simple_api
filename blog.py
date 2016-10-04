b807fb13465b1698436428112da8a2be651b9b7f
chenx <bufubaoni@163.com>
2016-10-04 20:45:05 +0800

    ajax finished

--- blog.py
+++ blog.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bottle import route, Bottle
+from bottle import route, Bottle, response
 import bottle
 from beaker.middleware import SessionMiddleware
 from config.sesion_config import sesion_config
+
 app = bottle.default_app()
 blog = SessionMiddleware(app, sesion_config)
 
 
 @route("/")
 def index():
-    return "This is index"
+    response.add_header(name="Access-Control-Allow-Origin", value="http://127.0.0.1:8000")
+    return dict({"title": "index",
+                 "content": "welcome to my blog"})
 
 
 @route("/topiclist")
@@ -19,4 +22,4 @@ def topiclist():
 
 
 if __name__ == "__main__":
-    bottle.run(app=blog, host="127.0.0.1", reloader=True, port=8000)
+    bottle.run(app=blog, host="127.0.0.1", reloader=True, port=8001)
