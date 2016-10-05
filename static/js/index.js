b66903c093504f077dbce402eb63f9b7c4ddeed9
chenx <bufubaoni@163.com>
2016-10-05 22:10:34 +0800

    add index.js for action

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,17 @@
 /**
  * Created by Alex on 2016/10/3.
  */
-const { Button } = require("rsuite");
-var Button = require("rsuite").Button;
-define(['rsuite'],function(Suite){
-    var Button = Suite.Button;
+var Index = Vue.extend({
+    template: "<div><h1>{{ msg.title }}</h1><p>{{ msg.content }}</p></div>",
+    data: function () {
+        return {
+            msg: $.ajax({
+                url: "http://127.0.0.1:8001",
+                dataType: "json",
+                async: false
+            }).done().responseJSON
+        }
+    }
 });
 
-ReactDOM.render(<Button>Button</Button>,mountNode);
+
