e44141740b3eb8c3df18b2cbcd3fb8ff2f621418
chenx <bufubaoni@163.com>
2016-10-06 23:34:45 +0800

    error list

--- static/js/index.js
+++ static/js/index.js
@@ -2,16 +2,22 @@
  * Created by Alex on 2016/10/3.
  */
 var Index = Vue.extend({
-    template: "<div><h1>{{ msg.title }}</h1><p>{{ msg.content }}</p></div>",
-    data: function () {
-        return {
-            msg: $.ajax({
-                url: "http://127.0.0.1:8001",
-                dataType: "json",
-                async: false
-            }).done().responseJSON
+        template: "<div><h1>{{ msg.title }}</h1><p>{{ msg.content }}</p></div>",
+        data: function () {
+            return {
+                msg: "123"
+            }
+        },
+        created: function () {
+            this.$http.get("http://127.0.0.1:8001/").then((response)=> {
+                console.log(response.data);
+                this.msg = response.data;
+                // this.$nextTick(function () {
+                //     this.msg = response.data;
+                // })
+            })
         }
-    }
-});
+    })
+    ;
 
 
--- static/js/topiclist.js
+++ static/js/topiclist.js
@@ -2,15 +2,18 @@
  * Created by Alex on 2016/10/5.
  */
 var Topiclist = Vue.extend({
-        template: "<table class='ui celled table'><tbody> <tr v-for = 'item in items'><td><router-link :to=\"{ name: 'topic', params: { id: item.id }}\"> {{ item.title }}</router-link></td></tr></tbody></table> ",
+        template: "<p>{{items}}</p><table class='ui celled table'><tbody> <tr v-for = 'item in items'><td><router-link :to=\"{ name: 'topic', params: { id: item.id }}\"> {{ item.title }}</router-link></td></tr></tbody></table> ",
         data: function () {
             return {
-                items: this.$http.get("http://127.0.0.1:8001/topiclist").then((response)=> {
-                    console.log(response.body);
-                    times = response.body;
-                })
+                items: []
             }
         },
+        created: function () {
+            this.$http.get("http://127.0.0.1:8001/topiclist").then((response)=> {
+                console.log(JSON.parse(response.data));
+                items = JSON.parse(response.data);
+            })
+        }
 
     })
     ;
