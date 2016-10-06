56339b147585359841e0d5dcaeb1b9f2bdd45ba5
chenx <bufubaoni@163.com>
2016-10-06 22:07:35 +0800

    use resource instead of jq

--- static/js/topiclist.js
+++ static/js/topiclist.js
@@ -2,14 +2,15 @@
  * Created by Alex on 2016/10/5.
  */
 var Topiclist = Vue.extend({
-    template: "<table class='ui celled table'><tbody> <tr v-for = 'item in items'><td><router-link :to=\"{ name: 'topic', params: { id: item.id }}\"> {{ item.title }}</router-link></td></tr></tbody></table> ",
-data: function () {
-    return {
-        items: $.ajax({
-            url: "http://127.0.0.1:8001/topiclist",
-            dataType: "json",
-            async: false
-        }).done().responseJSON
-    }
-}
-});
+        template: "<table class='ui celled table'><tbody> <tr v-for = 'item in items'><td><router-link :to=\"{ name: 'topic', params: { id: item.id }}\"> {{ item.title }}</router-link></td></tr></tbody></table> ",
+        data: function () {
+            return {
+                items: this.$http.get("http://127.0.0.1:8001/topiclist").then((response)=> {
+                    console.log(response.body);
+                    times = response.body;
+                })
+            }
+        },
+
+    })
+    ;
