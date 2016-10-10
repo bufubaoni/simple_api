2bf65057824f0dea46afabcaffc2c311fe67b5ca
chenx <bufubaoni@163.com>
2016-10-10 20:41:54 +0800

    topic view template

--- blog.py
+++ blog.py
@@ -31,7 +31,8 @@ def topiclist():
 def topic(topicid=None):
     response.add_header(name="Access-Control-Allow-Origin",
                         value="http://127.0.0.1:8000")
-    topic={"topic": "this is topic {topicid}".format(topicid=topicid)}
+    topic = {"topic": "this is topic {topicid}".format(topicid=topicid),
+             "title": "title"}
     return topic
 
 
--- static/js/topic.js
+++ static/js/topic.js
@@ -2,11 +2,20 @@
  * Created by Alex on 2016/10/5.
  */
 var Topic = Vue.extend({
-    template: "<div><h1>{{ topic.topic }}</h1></div>",
+    template: "<div class='row'>" +
+    "<div class='three wide column'>" + "<img>" + "</div>" +
+    "<div class='ten wide column'>" +
+    "<div>" +
+    "<h1>{{ topic.title }}</h1>" +
+    "<div class='ui divider'></div>" +
+    "<p>{{topic.topic}}</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>",
     data: function () {
         return {
             topic: $.ajax({
-                url: "http://127.0.0.1:8001/topic/"+this.$route.params.id,
+                url: "http://127.0.0.1:8001/topic/" + this.$route.params.id,
                 dataType: "json",
                 async: false
             }).done().responseJSON
