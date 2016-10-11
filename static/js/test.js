e8ec61f68f57129b8396289dca6384f3b5bb0dbb
chenx <bufubaoni@163.com>
2016-10-11 22:13:27 +0800

    add test perant to child

---
+++ static/js/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alex on 2016/10/11.
+ */
+var Test = Vue.extend({
+    template: "<h2>{{test}}</h2>>",
+    data: function () {
+        return {
+            test: "1233333"
+        }
+    }
+});
