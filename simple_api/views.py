fbb0423ff8649b9c2e920b1ca041cfc83a24d05c
chenxin <bufubaoni@163.com>
2017-12-26 14:56:00 +0800

    start app

--- simple_api/views.py
+++ simple_api/views.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.views.generic import View
+from django.http import HttpResponse
+import json
+
+
+class APIView(View):
+    json_engine = json
+
+    def dispatch(self, request, *args, **kwargs):
+        pass
